406d0e73be70aa081cf03fc2dd79ed7c8b36a207
Shahram Roohani <shahram.roohani@gbtsolutions.pt>
2023-10-02 08:47:56 +0100

    Add four pane view structure #3

--- src/cpp/mainwindow.cpp
+++ src/cpp/mainwindow.cpp
@@ -1,11 +1,25 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QSplitter>
+#include <QTableWidget>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
   ui->setupUi(this);
+
+  m_hSplitter      = new QSplitter(this);
+  m_vSplitterLeft  = new QSplitter(m_hSplitter);
+  m_vSplitterRight = new QSplitter(m_hSplitter);
+  m_vSplitterLeft->setOrientation(Qt::Vertical);
+  m_vSplitterRight->setOrientation(Qt::Vertical);
+  m_lContents = new QTableWidget(m_vSplitterLeft);
+  m_lProps = new QTableWidget(m_vSplitterLeft);
+  m_rContents = new QTableWidget(m_vSplitterRight);
+  m_rProps = new QTableWidget(m_vSplitterRight);
+  setCentralWidget(m_hSplitter);
 }
 
 MainWindow::~MainWindow()
--- src/cpp/mainwindow.h
+++ src/cpp/mainwindow.h
@@ -7,6 +7,9 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
+class QSplitter;
+class QTableWidget;
+
 class MainWindow : public QMainWindow
 {
   Q_OBJECT
@@ -17,5 +20,12 @@ public:
 
 private:
   Ui::MainWindow *ui;
+  QSplitter *m_hSplitter{},
+            *m_vSplitterLeft{},
+            *m_vSplitterRight{};
+  QTableWidget *m_lContents{},
+               *m_lProps{},
+               *m_rContents{},
+               *m_rProps{};
 };
 #endif // MAINWINDOW_H
